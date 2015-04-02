b0a0c4a80cae38149215f46e2c154273a863d674
Logan Koester <logan@logankoester.com>
2015-04-02 11:15:58 -0400

    Adds registrator recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,7 @@ default['docker-simple']['options'] = {}
 default['docker-simple']['options']['ip'] = '0.0.0.0'
 default['docker-simple']['options']['graph'] = '/var/lib/docker'
 
+default['docker-simple']['registrator']['tag'] = 'latest'
+default['docker-simple']['registrator']['command'] = '-h $HOSTNAME consul:'
+
 default['containers'] = []
---
+++ recipes/registrator.rb
@@ -0,0 +1,40 @@
+container_path = '/var/lib/container/registrator'
+
+tag = node['docker-simple']['registrator']['tag']
+ip = node['docker-simple']['registrator']['ip']
+command = node['docker-simple']['registrator']['command']
+
+compose = {
+  'registrator': {
+    'image': "gliderlabs/registrator:#{tag}",
+    'command': command,
+    'volumes': [
+      '/var/run/docker.sock:/tmp/docker.sock'
+    ]
+  }
+}
+
+directory container_path do
+  owner 'root'
+  group 'docker'
+  mode '0644'
+  recursive true
+  action :create
+end
+
+chef_gem 'psych'
+require 'psych'
+
+file "#{container_path}/docker-compose.yml" do
+  owner 'root'
+  group 'docker'
+  mode '0644'
+  content Psych.dump(compose)
+  action :create
+end
+
+bash 'Create and start container for registrator' do
+  cwd container_path
+  user 'root'
+  code 'docker-compose up -d --no-color --no-recreate'
+end
