61e344d4bdedc6b01fe4006bcfde09fa283325ba
Logan Koester <logan@logankoester.com>
2015-11-28 17:01:11 -0500

    Adds 'host' option

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default['docker-simple']['options'] = {}
 
 default['docker-simple']['options']['ip'] = '0.0.0.0'
 default['docker-simple']['options']['graph'] = '/var/lib/docker'
+default['docker-simple']['options']['host'] = 'fd://'
 
 default['docker-simple']['registrator']['tag'] = 'latest'
 default['docker-simple']['registrator']['command'] = '-h $HOSTNAME consul:'
