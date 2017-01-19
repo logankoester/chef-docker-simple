be4338b52cb1b2eeec5583c8d8ff83a2a5089af1
Logan Koester <logan@logankoester.com>
2017-01-19 09:20:23 +0000

    Adds cluster_store option

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default['docker-simple']['options'] = {}
 default['docker-simple']['options']['ip'] = '0.0.0.0'
 default['docker-simple']['options']['graph'] = '/var/lib/docker'
 default['docker-simple']['options']['host'] = 'fd://'
+default['docker-simple']['options']['cluster_store'] = 'consul://127.0.0.1:8500/swarm'
 
 default['docker-simple']['registrator']['tag'] = 'latest'
 default['docker-simple']['registrator']['command'] = '-h $HOSTNAME consul:'
