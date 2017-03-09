c919435ccea2ad4dbe64b09da57e7f41183e59fa
Logan Koester <logan@logankoester.com>
2017-03-09 18:27:08 +0000

    Bump 0.4.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'logan@logankoester.com'
 license 'MIT License'
 description 'Installs/Configures Docker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.3'
+version '0.4.4'
 
 depends 'pacman'
 supports 'arch'
